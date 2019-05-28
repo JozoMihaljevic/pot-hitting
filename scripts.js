9ed35cbeb9931ef5526ff9f8f7d32de0b15ae573
Jozo Mihaljevic <jozo.mihaljevic.cox4@gmail.com>
2019-05-28 12:43:23 +0200

    Refactored game without canvas

--- scripts.js
+++ scripts.js
@@ -1,20 +1,23 @@
 function startGame() {
-    let counter = 0;
-    const countDiv = document.getElementById("counter");
+    const myGameArea = document.getElementById("area");
+    const myGamePiece = document.getElementById("square");
+    myGamePiece.style.marginLeft = Math.floor((Math.random() * 580) + 1) + "px";
+    myGamePiece.style.marginTop = Math.floor((Math.random() * 480) + 1) + "px";
     const message = document.getElementById("message");
     const win = document.getElementById("win");
+    let counter = 0;
     let attempts = [600];
 
-    myGameArea.start();
-    myGameArea.canvas.addEventListener('click', function (event) {
+    myGamePiece.addEventListener("click", function () {
         counter++;
-        let distance = showDistance(event.x, event.y, myGamePiece.x, myGamePiece.y);
-        attempts.push(distance);
+        myGamePiece.style.background = "blue";
+        win.innerHTML = `You made it in ${counter} clicks!`;
+    }, false);
 
-        if (distance < 30) {
-            win.innerHTML = 'Win';
-            counter = 0;
-        }
+    myGameArea.addEventListener('click', function (event) {
+        counter++;
+        let distance = showDistance(event.x, event.y, myGamePiece.offsetLeft, myGamePiece.offsetTop);
+        attempts.push(distance);
 
         if (attempts.length < 3) {
             message.innerHTML = '';
@@ -23,35 +26,15 @@ function startGame() {
         } else {
             message.innerHTML = 'Hot';
         }
-
-        countDiv.innerHTML = counter;
     }, false);
-    myGamePiece = new component(30, 30, "blue");
-
-}
-
-const myGameArea = {
-    canvas: document.createElement("canvas"),
-    start: function () {
-        this.canvas.width = 500;
-        this.canvas.height = 600;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    },
-}
-
-function component(width, height, color) {
-    this.width = width;
-    this.height = height;
-    this.x = Math.floor((Math.random() * 480) + 1);
-    this.y = Math.floor((Math.random() * 580) + 1);
-    ctx = myGameArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
 }
 
 function showDistance(clickX, clickY, potX, potY) {
     const distance = Math.sqrt(Math.pow((potX - clickX), 2) +
         Math.pow((potY - clickY), 2));
     return distance;
+}
+
+function reset() {
+    location.reload();
 }
